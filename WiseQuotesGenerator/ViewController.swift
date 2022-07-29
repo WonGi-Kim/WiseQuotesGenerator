fcea2f7588bd863539ca16d419d85572ba6a8daf
WonGi-Kim <kwg970310@gmail.com>
2022-07-29 18:24:35 +0900

    Initial Commit

---
+++ WiseQuotesGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WiseQuotesGenerator
+//
+//  Created by 김원기 on 2022/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
